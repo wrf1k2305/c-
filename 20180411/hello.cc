4359585e8a1ea73609c92db89f610eb1d8b36a2c
wrf1k2305 <710162117@qq.com>
2018-04-11 09:24:55 +0800

    类

---
+++ 20180411/hello.cc
@@ -0,0 +1,14 @@
+ ///
+ /// @file    hello.cc
+ /// @author  wrf1k2305(710162117@qq.com)
+ /// @date    2018-04-11 09:23:13
+ ///
+ 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello world!" << endl;
+}
